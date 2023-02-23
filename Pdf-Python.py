7cc29c19b1a70858dd944c97dd2a1ca2da506ca3
KratiKhandelwal <105779349+KratiKhandelwal@users.noreply.github.com>
2023-02-23 13:43:29 +0530

    Create Pdf-Python.py
    1st Task - extracting data from PDF to Text with Python

---
+++ Pdf-Python.py
@@ -0,0 +1,29 @@
+import PyPDF2
+
+reader = PyPDF2.PdfReader('Atomic Habits.pdf') #read the pdf file
+page = reader.pages[3] # reading page by giving a specific page number
+print(page.extractText()) # used to extract text from the page
+
+#print(reader.getNumPages())
+#print(reader.getPage(3).extract_text())
+
+# Retrieve 10 pages text from a pdf .
+
+# Option 1 : get 10 pages and store in a text file
+import logging
+logging.basicConfig(filename='Reader.txt',level=logging.INFO , encoding= 'utf-8')
+
+str = ''
+for i in range(1,11):
+        str = str + '\n' + reader.getPage(i).extract_text()
+
+logging.info(str)
+
+# Option 2 : whole pdf to store in text file
+
+k = ''
+for i in range(reader.getNumPages()):
+        k = k+ '\n' + reader.getPage(i).extract_text()
+
+with open('Text.txt','w',encoding='utf-8') as l:
+        l.write(k)
